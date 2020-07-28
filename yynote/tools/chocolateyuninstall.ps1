4b2d063b82f3cd7476a8b1446c45dbfbc90c92a6
tshanli <tshanli@outlook.com>
2020-07-28 15:51:15 +0200

    Revert "Add yynote"
    This reverts commit 31aa35ffa95d7a5e22690ba16e4a08951161561b.

--- yynote/tools/chocolateyuninstall.ps1
+++
@@ -1,5 +0,0 @@
-﻿$ErrorActionPreference = 'Stop';
-$shortcutsPath = [System.Environment]::GetFolderPath('Programs')
-
-Remove-Item -Path (Join-Path -Path $shortcutsPath -ChildPath 'YYNote.lnk') -ErrorAction SilentlyContinue
-Remove-Item -Path (Join-Path -Path $env:ChocolateyToolsLocation -ChildPath 'YYNote') -Recurse -Force -ErrorAction SilentlyContinue
